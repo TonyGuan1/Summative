d2a93934934fd92f6e6276e30d61d593dd402568
Tony <tonyguan123@Hotmail.com>
2017-12-07 19:34:18 -0500

    Add files via upload

---
+++ Summative/src/summative/main/RunLibrarySystem.java
@@ -0,0 +1,13 @@
+package summative.main;
+
+import summative.controller.LibrarySystem;
+
+public class RunLibrarySystem {
+
+	public static void main(String[] args){
+		
+		new LibrarySystem();
+		
+	}
+	
+}
